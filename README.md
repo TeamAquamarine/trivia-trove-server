67963caf8445ceac8e650752e9534d04d8930da6
Connor <cacrossley22@gmail.com>
2018-08-13 13:24:38 -0700

    server scaffolding complete

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Server-side application for final 301 project
 
 ##Change Log
 - **8/13/18 1:05 pm** - Added dependencies
+- **8/13/18 1:25 pm** - Server scaffolding
--- server.js
+++ server.js
@@ -1,2 +1,42 @@
 'use strict';
 
+/***********************************
+*           DEPENDENCIES           *
+************************************/
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const superagent = require('superagent');
+
+/***********************************
+ *           SERVER SETUP           *
+ ************************************/
+const app = espress();
+const PORT = process.env.PORT;
+
+/***********************************
+*          DATABASE SETUP          *
+************************************/
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+
+/***********************************
+*           MIDDLEWARE             *
+************************************/
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+/***********************************
+*          API ENDPOINTS           *
+************************************/
+
+
+
+/***********************************
+*              LISTEN              *
+************************************/
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
