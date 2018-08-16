b1bb8da3b97b8065f4aa867e9a0fb6b9735a80b8
Connor <cacrossley22@gmail.com>
2018-08-15 21:31:57 -0700

    References #4. remove || 3000 form PORT config var

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const superagent = require('superagent');
  *           SERVER SETUP           *
  ************************************/
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 /***********************************
 *          DATABASE SETUP          *
